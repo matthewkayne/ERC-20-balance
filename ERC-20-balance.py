048f4968a5826de50363722d6fadfd0d0c9aa09b
Matthew Kayne <matthewkayne2004@gmail.com>
2022-03-21 00:01:02 +0000

    added docstring and snake case

--- ERC-20-balance.py
+++ ERC-20-balance.py
@@ -1,3 +1,4 @@
+"""ERC-20 Balance"""
 import tkinter as tk
 from tkinter import *
 from web3 import Web3
@@ -12,24 +13,27 @@ load_dotenv()
 w3 = os.getenv('ETH_URL')
 web3 = Web3(Web3.HTTPProvider(w3))
 
-addressList = list(csv.reader(open("Tokens.csv"))) 
-addressList=sorted(addressList, key=lambda x: x[0])
+address_list = list(csv.reader(open("Tokens.csv")))
+address_list = sorted(address_list, key=lambda x: x[0])
 
 address = None
 contract = None
 API_KEY = os.getenv('API_KEY')
 
-def getContract(address):
-    
+
+def get_contract(address):
+    """Get Contract"""
     global contract
-    
-    url = "https://api.etherscan.io/api?module=contract&action=getabi&address="+address+"&apikey="+API_KEY
+
+    url = "https://api.etherscan.io/api?module=contract&action=getabi&address=" + \
+        address+"&apikey="+API_KEY
 
     response = urlopen(url)
-    abi=json.loads(response.read())["result"]
-    contract = web3.eth.contract(address=address, abi=abi) 
+    abi = json.loads(response.read())["result"]
+    contract = web3.eth.contract(address=address, abi=abi)
 
-# Sets up GUI    
+
+# Sets up GUI
 window = tk.Tk()
 window.title("ERC-20 Balance")
 window.geometry("350x250")
@@ -37,66 +41,73 @@ window.geometry("350x250")
 # Allows the esc key to be used to close the GUI
 window.bind('<Escape>', lambda e: window.quit())
 
-nameList = []
-for i in range(len(addressList)):
-    nameList.append(addressList[i][0])
+name_list = []
+for i in range(len(address_list)):
+    name_list.append(address_list[i][0])
 
 name = StringVar(window)
-name.set(nameList[0]) # default value
+name.set(name_list[0])  # default value
 
-drop = OptionMenu(window, name, *nameList)
+drop = OptionMenu(window, name, *name_list)
 drop.pack()
 
-symbol=None
-inAddress=None
-userInfo=None
-infoLabel=None
+symbol = None
+in_address = None
+user_info = None
+info_label = None
+
 
 def getInfo():
-    
+
     global name
     global symbol
-    global inAddress
-    global userInfo
-    global infoLabel
-    
+    global in_address
+    global user_info
+    global info_label
+
     name = name.get()
-    contractAddress=None
-    for i in range(len(addressList)):
-        if name == addressList[i][0]:
-            contractAddress = addressList[i][1]
-    getContract(Web3.toChecksumAddress(contractAddress))
-                
+    contract_address = None
+    for i in range(len(address_list)):
+        if name == address_list[i][0]:
+            contract_address = address_list[i][1]
+    get_contract(Web3.toChecksumAddress(contract_address))
+
     info.destroy()
     drop.destroy()
-        
-    totalSupplyWei = contract.functions.totalSupply().call()
-    totalSupply = web3.fromWei(totalSupplyWei, 'ether')
-    symbol = contract.functions.symbol().call()
-    infoLabel = Label(window, text = f"Total Supply: {round(totalSupply)} {symbol} ")
-    infoLabel.pack()
-    inAddress= Entry(window, width= 25)
-    inAddress.pack()
-    userInfo = tk.Button(window, text="Get address Info", height = 2, width = 15, command = getBalance)
-    userInfo.pack()
-    
-def getBalance():
-    
-    infoLabel.destroy()
-    
-    checkSumAddress = Web3.toChecksumAddress(inAddress.get())
-    balanceWei = contract.functions.balanceOf(checkSumAddress).call()
-    balance = web3.fromWei(balanceWei, 'ether')
-    addressLabel = Label(window, text = checkSumAddress, font='Helvetica 13 bold')
-    addressLabel.pack()
-    
-    inAddress.destroy()
-    userInfo.destroy()
-    
-    balanceLabel = Label(window, text = f"Your Balance: {balance} {symbol}")
-    balanceLabel.pack()
 
-info = tk.Button(window,text="Get Info", height = 2, width = 10, command = getInfo)
+    total_supply_wei = contract.functions.total_supply().call()
+    total_supply = web3.fromWei(total_supply_wei, 'ether')
+    symbol = contract.functions.symbol().call()
+    info_label = Label(
+        window, text=f"Total Supply: {round(total_supply)} {symbol} ")
+    info_label.pack()
+    in_address = Entry(window, width=25)
+    in_address.pack()
+    user_info = tk.Button(window, text="Get address Info",
+                          height=2, width=15, command=get_balance)
+    user_info.pack()
+
+
+def get_balance():
+    """Get Balance"""
+
+    info_label.destroy()
+
+    check_sum_address = Web3.toChecksumAddress(in_address.get())
+    balance_wei = contract.functions.balanceOf(check_sum_address).call()
+    balance = web3.fromWei(balance_wei, 'ether')
+    address_label = Label(window, text=check_sum_address,
+                          font='Helvetica 13 bold')
+    address_label.pack()
+
+    in_address.destroy()
+    user_info.destroy()
+
+    balance_label = Label(window, text=f"Your Balance: {balance} {symbol}")
+    balance_label.pack()
+
+
+info = tk.Button(window, text="Get Info", height=2, width=10, command=getInfo)
 info.pack()
 
 tk.mainloop()
